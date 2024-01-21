d5e2296efaae3f6fa461a6e33478c1a33ee784d0
Sean van der Merwe <111183630+Thunder2505@users.noreply.github.com>
2024-01-21 13:45:58 +0200

    Release
    PassLock release and dependencies added

---
+++ PassLock-Release/moc_passlock.cpp
@@ -0,0 +1,388 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'passlock.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../PassLock-Desktop/passlock.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'passlock.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSPassLockENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSPassLockENDCLASS = QtMocHelpers::stringData(
+    "PassLock",
+    "on_btnAddSave_clicked",
+    "",
+    "on_edtViewSearch_returnPressed",
+    "on_btnViewSearch_clicked",
+    "PasswordWin",
+    "SetFav",
+    "on_twPassLock_currentChanged",
+    "index",
+    "on_btnQVFavEmail_clicked",
+    "on_btnQVFavUname_clicked",
+    "on_btnQVFavPassword_clicked",
+    "on_btnQVFreqEmail_clicked",
+    "on_btnQVFreqUname_clicked",
+    "on_btnQVFreqPassword_clicked",
+    "on_btnQVRecEmail_clicked",
+    "on_btnQVRecUname_clicked",
+    "on_btnQVRecPassword_clicked",
+    "on_btnAddRC_clicked",
+    "on_btnRCDone_clicked",
+    "on_btnRCAdd_clicked",
+    "on_tabwRCList_cellClicked",
+    "row",
+    "on_btnRCRem_clicked",
+    "deleteAcc",
+    "copyFromTable",
+    "column",
+    "editPassword",
+    "saveChanges",
+    "showPasswordDialog",
+    "ChangeDbDir"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSPassLockENDCLASS_t {
+    uint offsetsAndSizes[62];
+    char stringdata0[9];
+    char stringdata1[22];
+    char stringdata2[1];
+    char stringdata3[31];
+    char stringdata4[25];
+    char stringdata5[12];
+    char stringdata6[7];
+    char stringdata7[29];
+    char stringdata8[6];
+    char stringdata9[25];
+    char stringdata10[25];
+    char stringdata11[28];
+    char stringdata12[26];
+    char stringdata13[26];
+    char stringdata14[29];
+    char stringdata15[25];
+    char stringdata16[25];
+    char stringdata17[28];
+    char stringdata18[20];
+    char stringdata19[21];
+    char stringdata20[20];
+    char stringdata21[26];
+    char stringdata22[4];
+    char stringdata23[20];
+    char stringdata24[10];
+    char stringdata25[14];
+    char stringdata26[7];
+    char stringdata27[13];
+    char stringdata28[12];
+    char stringdata29[19];
+    char stringdata30[12];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSPassLockENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSPassLockENDCLASS_t qt_meta_stringdata_CLASSPassLockENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 8),  // "PassLock"
+        QT_MOC_LITERAL(9, 21),  // "on_btnAddSave_clicked"
+        QT_MOC_LITERAL(31, 0),  // ""
+        QT_MOC_LITERAL(32, 30),  // "on_edtViewSearch_returnPressed"
+        QT_MOC_LITERAL(63, 24),  // "on_btnViewSearch_clicked"
+        QT_MOC_LITERAL(88, 11),  // "PasswordWin"
+        QT_MOC_LITERAL(100, 6),  // "SetFav"
+        QT_MOC_LITERAL(107, 28),  // "on_twPassLock_currentChanged"
+        QT_MOC_LITERAL(136, 5),  // "index"
+        QT_MOC_LITERAL(142, 24),  // "on_btnQVFavEmail_clicked"
+        QT_MOC_LITERAL(167, 24),  // "on_btnQVFavUname_clicked"
+        QT_MOC_LITERAL(192, 27),  // "on_btnQVFavPassword_clicked"
+        QT_MOC_LITERAL(220, 25),  // "on_btnQVFreqEmail_clicked"
+        QT_MOC_LITERAL(246, 25),  // "on_btnQVFreqUname_clicked"
+        QT_MOC_LITERAL(272, 28),  // "on_btnQVFreqPassword_clicked"
+        QT_MOC_LITERAL(301, 24),  // "on_btnQVRecEmail_clicked"
+        QT_MOC_LITERAL(326, 24),  // "on_btnQVRecUname_clicked"
+        QT_MOC_LITERAL(351, 27),  // "on_btnQVRecPassword_clicked"
+        QT_MOC_LITERAL(379, 19),  // "on_btnAddRC_clicked"
+        QT_MOC_LITERAL(399, 20),  // "on_btnRCDone_clicked"
+        QT_MOC_LITERAL(420, 19),  // "on_btnRCAdd_clicked"
+        QT_MOC_LITERAL(440, 25),  // "on_tabwRCList_cellClicked"
+        QT_MOC_LITERAL(466, 3),  // "row"
+        QT_MOC_LITERAL(470, 19),  // "on_btnRCRem_clicked"
+        QT_MOC_LITERAL(490, 9),  // "deleteAcc"
+        QT_MOC_LITERAL(500, 13),  // "copyFromTable"
+        QT_MOC_LITERAL(514, 6),  // "column"
+        QT_MOC_LITERAL(521, 12),  // "editPassword"
+        QT_MOC_LITERAL(534, 11),  // "saveChanges"
+        QT_MOC_LITERAL(546, 18),  // "showPasswordDialog"
+        QT_MOC_LITERAL(565, 11)   // "ChangeDbDir"
+    },
+    "PassLock",
+    "on_btnAddSave_clicked",
+    "",
+    "on_edtViewSearch_returnPressed",
+    "on_btnViewSearch_clicked",
+    "PasswordWin",
+    "SetFav",
+    "on_twPassLock_currentChanged",
+    "index",
+    "on_btnQVFavEmail_clicked",
+    "on_btnQVFavUname_clicked",
+    "on_btnQVFavPassword_clicked",
+    "on_btnQVFreqEmail_clicked",
+    "on_btnQVFreqUname_clicked",
+    "on_btnQVFreqPassword_clicked",
+    "on_btnQVRecEmail_clicked",
+    "on_btnQVRecUname_clicked",
+    "on_btnQVRecPassword_clicked",
+    "on_btnAddRC_clicked",
+    "on_btnRCDone_clicked",
+    "on_btnRCAdd_clicked",
+    "on_tabwRCList_cellClicked",
+    "row",
+    "on_btnRCRem_clicked",
+    "deleteAcc",
+    "copyFromTable",
+    "column",
+    "editPassword",
+    "saveChanges",
+    "showPasswordDialog",
+    "ChangeDbDir"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSPassLockENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      26,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  170,    2, 0x08,    1 /* Private */,
+       3,    0,  171,    2, 0x08,    2 /* Private */,
+       4,    0,  172,    2, 0x08,    3 /* Private */,
+       5,    0,  173,    2, 0x08,    4 /* Private */,
+       6,    0,  174,    2, 0x08,    5 /* Private */,
+       7,    1,  175,    2, 0x08,    6 /* Private */,
+       9,    0,  178,    2, 0x08,    8 /* Private */,
+      10,    0,  179,    2, 0x08,    9 /* Private */,
+      11,    0,  180,    2, 0x08,   10 /* Private */,
+      12,    0,  181,    2, 0x08,   11 /* Private */,
+      13,    0,  182,    2, 0x08,   12 /* Private */,
+      14,    0,  183,    2, 0x08,   13 /* Private */,
+      15,    0,  184,    2, 0x08,   14 /* Private */,
+      16,    0,  185,    2, 0x08,   15 /* Private */,
+      17,    0,  186,    2, 0x08,   16 /* Private */,
+      18,    0,  187,    2, 0x08,   17 /* Private */,
+      19,    0,  188,    2, 0x08,   18 /* Private */,
+      20,    0,  189,    2, 0x08,   19 /* Private */,
+      21,    1,  190,    2, 0x08,   20 /* Private */,
+      23,    0,  193,    2, 0x08,   22 /* Private */,
+      24,    0,  194,    2, 0x08,   23 /* Private */,
+      25,    2,  195,    2, 0x08,   24 /* Private */,
+      27,    0,  200,    2, 0x08,   27 /* Private */,
+      28,    0,  201,    2, 0x08,   28 /* Private */,
+      29,    0,  202,    2, 0x08,   29 /* Private */,
+      30,    0,  203,    2, 0x08,   30 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   22,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   22,   26,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject PassLock::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSPassLockENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSPassLockENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSPassLockENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<PassLock, std::true_type>,
+        // method 'on_btnAddSave_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_edtViewSearch_returnPressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnViewSearch_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'PasswordWin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'SetFav'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_twPassLock_currentChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_btnQVFavEmail_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVFavUname_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVFavPassword_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVFreqEmail_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVFreqUname_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVFreqPassword_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVRecEmail_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVRecUname_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnQVRecPassword_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnAddRC_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnRCDone_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnRCAdd_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_tabwRCList_cellClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_btnRCRem_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'deleteAcc'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'copyFromTable'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'editPassword'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'saveChanges'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'showPasswordDialog'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'ChangeDbDir'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void PassLock::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<PassLock *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_btnAddSave_clicked(); break;
+        case 1: _t->on_edtViewSearch_returnPressed(); break;
+        case 2: _t->on_btnViewSearch_clicked(); break;
+        case 3: _t->PasswordWin(); break;
+        case 4: _t->SetFav(); break;
+        case 5: _t->on_twPassLock_currentChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 6: _t->on_btnQVFavEmail_clicked(); break;
+        case 7: _t->on_btnQVFavUname_clicked(); break;
+        case 8: _t->on_btnQVFavPassword_clicked(); break;
+        case 9: _t->on_btnQVFreqEmail_clicked(); break;
+        case 10: _t->on_btnQVFreqUname_clicked(); break;
+        case 11: _t->on_btnQVFreqPassword_clicked(); break;
+        case 12: _t->on_btnQVRecEmail_clicked(); break;
+        case 13: _t->on_btnQVRecUname_clicked(); break;
+        case 14: _t->on_btnQVRecPassword_clicked(); break;
+        case 15: _t->on_btnAddRC_clicked(); break;
+        case 16: _t->on_btnRCDone_clicked(); break;
+        case 17: _t->on_btnRCAdd_clicked(); break;
+        case 18: _t->on_tabwRCList_cellClicked((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 19: _t->on_btnRCRem_clicked(); break;
+        case 20: _t->deleteAcc(); break;
+        case 21: _t->copyFromTable((*reinterpret_cast< std::add_pointer_t<int>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2]))); break;
+        case 22: _t->editPassword(); break;
+        case 23: _t->saveChanges(); break;
+        case 24: _t->showPasswordDialog(); break;
+        case 25: _t->ChangeDbDir(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *PassLock::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *PassLock::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSPassLockENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int PassLock::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 26)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 26;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 26)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 26;
+    }
+    return _id;
+}
+QT_WARNING_POP
